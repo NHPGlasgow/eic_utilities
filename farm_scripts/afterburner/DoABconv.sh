1646c527dd7c86167ff38fb5923022865483a807
garypenman <g.penman.1@research.gla.ac.uk>
2023-07-07 15:29:19 +0100

    Wrote DoABconv.sh afterburner script for general use, with help messages and default settings.

--- farm_scripts/afterburner/DoABconv.sh
+++ farm_scripts/afterburner/DoABconv.sh
@@ -1,32 +1,92 @@
 #!/bin/bash
 
-#if [ $# == 0 ];
-#then
-#    echo "No arguments given"
-#    exit 2
-#fi
+print_usage(){
+    echo " "
+    echo "Usage: DoABconv.sh [path-to-files] [config] [n-segments] [nevents]"
+    echo "n-segments: number of segments for file to be split into."
+    echo "nevents: number of events per segment"
+    echo " "
+    echo " -h    print this message"
+    echo " -c    print abconv help and config information"
+    echo " -d    run script with default settings"
+    
+}
 
-#config=$1
-config="5x41"
+print_configs(){
+    abconv -h
+}
 
-basedir="/w/work5/home/garyp/eic/Generators/Topeg/HepMC/June_2022"
 
-file=$basedir"/eA-"$config"-M3-Ph.hepmc"
-
-if [ ! -f $file ];
+if [ "$1" == "-h" ];
 then
-    echo "File not found, check config."
+    print_usage
+    exit 0
+elif [ "$1" == "-c" ];
+then
+    print_configs
+    exit 0
+elif [ "$1" == "-d" ];
+then
+    echo "Running defaults"
+    #=0
+elif [ $# == 0 ];
+then
+    echo "No arguments given. Running defaults."
+    print_usage
     exit 2
 fi
 
-nseg=100
-nevent=10000
-for((seg=0; seg<$nseg; seg++));
+
+    
+if [ $# -ge 1 ] && [ "$1" != "-d" ]
+then
+    basedir=$1
+else
+    basedir="/w/work5/eic/NHP_test/data"
+fi
+
+if [ $# -ge 2 ]
+then
+    config=$1
+else 
+    config=0 #default abconv config, 0==IP6 High divergence, auto read energy
+fi
+
+if [ $# -ge 3 ]
+then 
+    nseg=$3
+else
+    nseg=10
+fi
+
+if [ $# -ge 4 ]
+then
+    nevents=$4
+else
+    nevents=10000
+fi
+    
+outdir=$basedir"/afterburned"
+mkdir -p ${outdir}
+
+for file in $basedir/*.hepmc
 do
-    firstevent=$(($seg*$nevent))
-    echo $firstevent
-    outfile="/scratch/garyp/Topeg_eHe4_"$config"_"$seg
-    abconv -p ip6_hidiv_41x5 -s $firstevent -l $nevent --plot-off $file -o $outfile
+    if [ ! -f $file ];
+    then
+	echo "No Files not found, check config."
+	exit 2
+    fi
+    
+    basefile=${file##*/}
+    basefile=${basefile%.hepmc}
+
+    for((seg=0; seg<$nseg; seg++));
+    do
+	firstevent=$(($seg*$nevents))
+	outfile=$outdir/"AB_"$basefile"_"$seg
+	echo "segment: $seg    first event: $firstevent    output file: $outfile"
+	abconv -p ip6_hidiv_41x5 -s $firstevent -l $nevents --plot-off $file -o $outfile
+    done
 done
 
-#abconv -p ip6_hidiv_41x5 -s 0 -e 99 --plot-off $file -o test
+    
