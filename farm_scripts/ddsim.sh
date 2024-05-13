ee34e2554b4221578218427a6f4f17448fc4a0a8
garypenman <g.penman.1@research.gla.ac.uk>
2024-05-13 13:51:04 +0100

    updated wrapper to have more failsafes, and not split up files twice. Updated ddsim.sh to do the afterburner and reconstruction on each segment. full analysis flow now takes in a filebase, looks for all files in that base, splits them into 10k segments, produces afterburn, simulation and recon for each job and then moves it all back to /work disks

--- farm_scripts/ddsim.sh
+++ farm_scripts/ddsim.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-source /opt/detector/epic-nightly/setup.sh
+#echo "Job started at $(date) on $(hostname)"
 
+source /opt/detector/epic-nightly/setup.sh
+source 
 tempdir=/scratch/$USER/ddsim_$BASENAME"_"$JOB
 mkdir -p ${tempdir}
+cd $tempdir
 
 datadir=${tempdir}/data
 logdir=${tempdir}/logs
@@ -13,27 +16,47 @@ mkdir -p ${datadir}
 mkdir -p ${logdir}
 mkdir -p ${outdir}
 
-cp $WORK_FILE $datadir
+#cp $WORK_FILE $datadir
 
 datafile=$datadir/$BASEFILE
+ABoutfile=$WORK_AB_DIR/AB_$BASENAME"_"$JOB.edm4hep.root
+if [[ ! -f $ABoutfile ]]
+then
+    #if afterburned segment doesnt exist in correct place, do it
+    abconv -p 1 -s $firstevent -l $nevent --plot-off $datafile -o $ABoutfile
+fi
+
 outfile=$outdir/$BASENAME"_"$JOB.edm4hep.root
 logfile=$logdir/$BASENAME"_"$JOB.log
+reconfile=$outdir/$BASENAME"_"$JOB"_recon.root"
+reconlogfile=$logdir/$BASENAME"_"$JOB"_recon.log"
 
-export JUGGLER_MC_FILE=$datafile
+
+##setup simulation input output based on AB output
+export JUGGLER_MC_FILE=$ABoutfile
 export JUGGLER_SIM_FILE=$outfile
-    
-nohup ddsim --steeringFile steering.py \
+
+#do simulation
+ddsim --steeringFile steering.py \
     --numberOfEvents ${JUGGLER_N_EVENTS} \
     --compactFile ${DETECTOR_PATH}/${DETECTOR_CONFIG}.xml \
     --inputFiles ${JUGGLER_MC_FILE}  \
     --outputFile  ${JUGGLER_SIM_FILE} \
+    -v 'WARNING' \
     >> $logfile 2>&1
 
-#clean up
+#do reconstruction
+eicrecon $JUGGLER_SIM_FILE >> $reconlogfile 2>&1
+
+mv podio_output.root $reconfile
+mv $reconfile $WORK_RECON_DIR
 mv $JUGGLER_SIM_FILE $WORK_OUT_DIR
 mv $logfile $WORK_LOG_DIR
+mv $reconlogfile $WORK_RECON_LOG_DIR
+
 rm $datafile
 rmdir $datadir $logdir $outdir
-rmdir $tempdir
+cd
+rm -rf $tempdir
 
-echo "Simulation $RUN $SEG complete!"
+echo "Simulation job $BASENAME $SEG complete at $(date) on $(hostname)!"
--- farm_scripts/wrapper_example.sh
+++ farm_scripts/wrapper_example.sh
@@ -1,63 +1,108 @@
 #!/bin/bash
 
-#G. Penman 2023
+#G. Penman 2024
+#new input arguments to control whether you want to use farm or local npc processing, how many runs/files/segments and then how many events per file. Default for 
+if [ $# -lt 2 ]
+then
+    echo "Please provide a batch-mode (farm or npc) and a filebase (i.e. EpiC_18x275_ep), and optionally a number of runs/files(default = -1 for all files), and number of events per run (default = 10k)."
+    exit 2
+fi
+
+batchmode=$1
+filebase=$2
+
+if [ $batchmode != "farm" ] && [ $batchmode != "npc" ]
+then
+    echo "Enter a valid batch option, farm or npc"
+    exit 2
+fi
+
+if [ $# -gt 2 ]
+then
+    nruns=$3
+else 
+    nruns=-1
+fi
+
+if [ $# -gt 3 ]
+then
+    neventsim=$4
+else
+    neventsim=10000
+fi
+
+
 #Set these as appropriate to user
 #In theory these lines should be the only needed changes.
 #As long as this script finds data in WORK_DATA_DIR, the rest SHOULD just about work in any scenario
-export WORK_DIR=/w/work5/eic/NHP_test
-export WORK_DATA_DIR=$WORK_DIR/data
-export WORK_LOG_DIR=$WORK_DIR/logs
-export WORK_OUT_DIR=$WORK_DIR/rootfiles
-export WORK_RECON_DIR=$WORK_DIR/recon
-export FARM_LOG_DIR=/home/$USER/ddsim_farm_logs
+export WORK_DIR=/w/work5/eic/NHP_farm
+export WORK_DATA_DIR=$WORK_DIR/data/$filebase
+export WORK_AB_DIR=$WORK_DIR/data_afterburned/$filebase
+export WORK_LOG_DIR=$WORK_DIR/logs/$filebase
+export WORK_OUT_DIR=$WORK_DIR/rootfiles/$filebase
+export WORK_RECON_DIR=$WORK_DIR/recon/$filebase
+export WORK_RECON_LOG_DIR=$WORK_DIR/recon_logs/$filebase
+export FARM_LOG_DIR=/home/$USER/ddsim_farm_logs/$filebase
 
 export SIM_DIR=$PWD
 
-if [ ! -d $WORK_DATA_DIR ]
+if [[ ! -d $WORK_DATA_DIR  ||  ! -n "$(ls -A $WORK_DATA_DIR)" ]] 
 then
-    echo "Data directory not valid. Check Paths"
+    echo "Data directory not valid or is empty. Check filebase and paths!!"
     exit 2
 fi
 
 #makes the output directories where stuff is moved to on /work in the case that the user hasnt already
+mkdir -p ${WORK_AB_DIR}
 mkdir -p ${WORK_LOG_DIR}
 mkdir -p ${WORK_OUT_DIR}
 mkdir -p ${WORK_RECON_DIR}
+mkdir -p ${WORK_RECON_LOG_DIR}
 mkdir -p ${FARM_LOG_DIR}
 
 #if you need to skip a certain number of events in a file for some reason. otherwise leave to 0 and let the below for loop do its thing
+run=0
 nskip=0
-#total number of events you want to simulate PER DATA FILE (leave small for testing scripts)
-neventstotal=10
-#number of events per job. recommend keep below 10000 per job.
-neventsim=10
+
+echo "\nSubmitting jobs for $nruns files in MC base $filebase"
 
 #Copy this script for yourself if you need to change the syntax of the job setup based on your file structure.
 #Variable NSKIP can be used to skip a given number of events each job if all data in one file for example
-for file in $WORK_DATA_DIR/EpiC_18x275_ep_0.hepmc*;
+for file in $WORK_DATA_DIR/*.hepmc;
 do
-    export WORK_FILE=$file
-    if [ neventstotal == -1 ]
+    if [[ $run -eq $nruns ]]
     then
-	export NEVENTS=$(grep -c "E " $file)
+	break;
     else
-	export NEVENTS=$neventstotal
+	run=$run+1
     fi
     
-    njobs=$(($NEVENTS / $neventsim))
+    #echo $file
+    export WORK_FILE=$file
+    
+    neventsfile=$(grep -c "E " $file)
+	
+    if [[ $neventsim -eq -1 ]]
+    then
+	export NJOBS=1
+	neventsim=$neventsfile
+    else
+	export NJOBS=$(($neventsfile / $neventsim))
+    fi
+    
     
     export BASEFILE=${file##*/}
     export BASENAME=${BASEFILE%.hepmc}
 
-    echo "Submitting $nevents events in  $njobs jobs for MC base: $BASENAME"
-    
-    for (( job=0; job<$njobs; job++))
-    do
 
-	export JOBNAME="EPIC_"$BASENAME"_"$job
+    echo "Submitting $NJOBS jobs with $neventsim events per job, for MC file: $BASEFILE"
+    
+    for (( job=0; job<$NJOBS; job++))
+    do
+	export JOBNAME="DDSIM_"$BASENAME"_"$job
 	export NSKIP=$(($neventsim * $job))
 	export JOB=$job
-	export JUGGLER_N_EVENTS=$NEVENTS
+	export JUGGLER_N_EVENTS=$neventsim
 	
 	echo "Job: $JOB. NSKIP: $NSKIP"
 	outfile=$WORK_OUT_DIR/$BASENAME"_"$JOB".edm4hep.root"
@@ -66,9 +111,16 @@ do
 	    echo "Output files exists already. Skipping job."
 	    continue
 	fi
-	qsub -V -q clas12 -e $FARM_LOG_DIR -o $FARM_LOG_DIR -N $JOBNAME jobexec.sh
-	#./jobexec.sh &
-	#run=$(( $run+1 ))
-	sleep 5
+
+	if [ $batchmode == "farm" ]
+	then
+	    echo "Farming"
+	    #qsub -V -q clas12 -e $FARM_LOG_DIR -o $FARM_LOG_DIR -N $JOBNAME jobexec.sh
+	elif [ $batchmode == "npc" ]
+	then
+	    echo "npcing"
+	    #./jobexec.sh &
+	fi
+	sleep 1
     done
 done
