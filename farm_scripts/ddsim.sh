880cc98776cc37b1c7c1d9dbc97772f3b0bd8f4d
garypenman <g.penman.1@research.gla.ac.uk>
2025-01-21 00:17:08 +0000

    Fixed erroneous errors left behind from rushed changes last year

--- farm_scripts/ddsim.sh
+++ farm_scripts/ddsim.sh
@@ -2,10 +2,11 @@
 
 #echo "Job started at $(date) on $(hostname)"
 
-source /opt/detector/epic-main/bin/thisepic.sh
-source /opt/local/bin/eicrecon-this.sh
+#source /opt/detector/epic-main/bin/thisepic.sh
+#source /opt/local/bin/eicrecon-this.sh
+source /home/garyp/eic/setup_local.sh
 
-tempdir=/scratch/$USER/ddsim_$BASENAME"_"$JOB
+tempdir=/scratch/$USER/ddsim_$BASENAME
 mkdir -p ${tempdir}
 cd $tempdir
 cp $STEERINGFILE $tempdir
@@ -22,22 +23,26 @@ cp $STEERINGFILE $tempdir
 
 #datafile=$datadir/$BASEFILE
 datafile=$WORK_FILE
-ABoutfile=$WORK_AB_DIR/AB_$BASENAME"_"$JOB.hepmc
+#ABoutfile=$WORK_AB_DIR/AB_$BASENAME
+ABoutfile=$tempdir"/AB_"$BASENAME
+ablogfile=$ABoutfile.log
 if [[ ! -f $ABoutfile ]]
 then
     #if afterburned segment doesnt exist in correct place, do it
-    echo "No afterburned datafile for Segment/Run $JOB, running afterburner" 
-    abconv -p 1 -s 0 -l -1 --plot-off $datafile -o $ABoutfile
+    #echo "No afterburned datafile for Segment/Run $JOB, running afterburner" 
+    abconv -p 1 -s $FIRSTEVENT -l $LASTEVENT --plot-off $datafile -o $ABoutfile >> $ablogfile 2>&1
 fi
 
-outfile=$tempdir/$BASENAME"_"$JOB.edm4hep.root
-logfile=$tempdir/$BASENAME"_"$JOB.log
-reconfile=$tempdir/$BASENAME"_"$JOB"_recon.root"
-reconlogfile=$tempdir/$BASENAME"_"$JOB"_recon.log"
+echo "AB complete at $(date) on $(hostname)"
+
+outfile=$tempdir"/"$BASENAME".edm4hep.root"
+logfile=$tempdir"/"$BASENAME".log"
+reconfile=$tempdir"/"$BASENAME"_recon.root"
+reconlogfile=$tempdir"/"$BASENAME"_recon.log"
 
 
 ##setup simulation input output based on AB output
-export JUGGLER_MC_FILE=$ABoutfile
+export JUGGLER_MC_FILE=$ABoutfile".hepmc"
 export JUGGLER_SIM_FILE=$outfile
 
 #do simulation
@@ -49,18 +54,22 @@ ddsim --steeringFile steering.py \
     -v 'WARNING' \
     >> $logfile 2>&1
 
+echo "Simulation complete at $(date) on $(hostname)"
+
 #do reconstruction
 eicrecon $JUGGLER_SIM_FILE >> $reconlogfile 2>&1
+
+echo "Reconstruction complete at $(date) on $(hostname)"
 
 mv podio_output.root $reconfile
 mv $reconfile $WORK_RECON_DIR
 mv $JUGGLER_SIM_FILE $WORK_OUT_DIR
 mv $logfile $WORK_LOG_DIR
 mv $reconlogfile $WORK_RECON_LOG_DIR
-
+mv $ablogfile $WORK_AB_LOG_DIR
 #rm $datafile
 #rmdir $datadir $logdir $outdir
 cd
-#rm -rf $tempdir
+rm -rf $tempdir
 
-echo "Simulation job $BASENAME $SEG complete at $(date) on $(hostname)!"
+echo "Job $BASENAME $SEG complete at $(date) on $(hostname)!"
--- farm_scripts/wrapper_example.sh
+++ farm_scripts/wrapper_example.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#G. Penman 2024
+#G. Penman 2025 (updated)
 #new input arguments to control whether you want to use farm or local npc processing, how many runs/files/segments and then how many events per file. Default for 
 if [ $# -lt 2 ]
 then
-    echo "Please provide a batch-mode (farm or npc) and a filebase (i.e. EpiC_18x275_ep), and optionally a number of runs/files(default = -1 for all files), and number of events per run (default = 10k)."
+    echo "Please provide a batch-mode (farm or npc) and a filebase (i.e. EpiC_18x275_ep), and optionally a number of runs/files(default = -1 for all files), segments to split each file into (default = -1 for file total ev / 10k), and number of events per segment (default = 10k)."
     exit 2
 fi
 
@@ -26,9 +26,16 @@ fi
 
 if [ $# -gt 3 ]
 then
-    neventsim=$4
+    nseg=$4
 else
-    neventsim=10000
+    nseg=-1
+fi
+
+if [ $# -gt 4 ]
+then
+    nevents=$5
+else
+    nevents=10000
 fi
 
 
@@ -36,8 +43,10 @@ fi
 #In theory these lines should be the only needed changes.
 #As long as this script finds data in WORK_DATA_DIR, the rest SHOULD just about work in any scenario
 export WORK_DIR=/w/work5/eic/NHP_farm
+#export WORK_DIR=/w/work5/home/garyp/eic/Farm/
 export WORK_DATA_DIR=$WORK_DIR/data/$filebase
-export WORK_AB_DIR=$WORK_DIR/data_afterburned/$filebase
+#export WORK_AB_DIR=$WORK_DIR/data_afterburned/$filebase
+export WORK_AB_LOG_DIR=$WORK_DIR/ab_logs/$filebase
 export WORK_LOG_DIR=$WORK_DIR/logs/$filebase
 export WORK_OUT_DIR=$WORK_DIR/rootfiles/$filebase
 export WORK_RECON_DIR=$WORK_DIR/recon/$filebase
@@ -53,9 +62,10 @@ then
 fi
 
 #makes the output directories where stuff is moved to on /work in the case that the user hasnt already
-mkdir -p ${WORK_AB_DIR}
-mkdir -p ${WORK_LOG_DIR}
+#mkdir -p ${WORK_AB_DIR}
+mkdir -p ${WORK_AB_LOG_DIR}
 mkdir -p ${WORK_OUT_DIR}
+mkdir -p ${WORK_LOG_DIR}
 mkdir -p ${WORK_RECON_DIR}
 mkdir -p ${WORK_RECON_LOG_DIR}
 mkdir -p ${FARM_LOG_DIR}
@@ -63,8 +73,12 @@ mkdir -p ${FARM_LOG_DIR}
 #if you need to skip a certain number of events in a file for some reason. otherwise leave to 0 and let the below for loop do its thing
 run=0
 nskip=0
+if [[ $nruns -eq -1 ]]
+then
+    nruns=`ls $WORK_DATA_DIR/*.hepmc | wc -l`
+fi
 
-echo "\nSubmitting jobs for $nruns files in MC base $filebase"
+echo "Submitting jobs for $nruns files in MC base $filebase"
 
 #Copy this script for yourself if you need to change the syntax of the job setup based on your file structure.
 #Variable NSKIP can be used to skip a given number of events each job if all data in one file for example
@@ -81,33 +95,52 @@ do
     export WORK_FILE=$file
     
     neventsfile=$(grep -c "E " $file)
-	
-    if [[ $neventsim -eq -1 ]]
+    
+    #give the user exactly how many segments and events per segment 
+    #that they specify
+    if [[ $nseg -gt 0 ]]
+    then
+	echo "doing exactly as many jobs as im told"
+	export NJOBS=$nseg
+	neventsim=$nevents
+    #unless they set nev=-1, in which case reset njobs back to 1
+    #and number events to simulate equal to total in that file
+    elif [[ $nevents -eq -1 ]]
     then
 	export NJOBS=1
 	neventsim=$neventsfile
+    #if they 
     else
-	#export NJOBS=$(($neventsfile / $neventsim))
-	export NJOBS=1
+	export NJOBS=$(($neventsfile / $nevents))
+	neventsim=$nevents
+	#for testing
+	#export NJOBS=1
     fi
     
     
     export BASEFILE=${file##*/}
-    export BASENAME=${BASEFILE%.hepmc}
+    basefile=${BASEFILE%.hepmc}
     export STEERINGFILE=$PWD"/steering.py"
 
     echo "Submitting $NJOBS jobs with $neventsim events per job, for MC file: $BASEFILE"
     
     for (( job=0; job<$NJOBS; job++))
     do
-	export JOBNAME="DDSIM_"$BASENAME"_"$job
+	if [[ $NJOBS -ne 1 ]]
+	then
+	    export BASENAME=$basefile"_"$job
+	fi
+	
+	export JOBNAME="DDSIM_"$BASENAME
 	export NSKIP=$(($neventsim * $job))
+	export FIRSTEVENT=$NSKIP
+	export LASTEVENT=$(($NSKIP + $neventsim))
 	export JOB=$job
 	export JUGGLER_N_EVENTS=$neventsim
 	
 	echo "Job: $JOB. NSKIP: $NSKIP"
-	outfile=$WORK_OUT_DIR/$BASENAME"_"$JOB".edm4hep.root"
-	if [ -f $outfile ]
+	testoutfile=$WORK_OUT_DIR/$BASENAME".edm4hep.root"
+	if [ -f $testoutfile ]
 	then
 	    echo "Output files exists already. Skipping job."
 	    continue
@@ -116,12 +149,13 @@ do
 	if [ $batchmode == "farm" ]
 	then
 	    echo "Farming"
-	    #qsub -V -q clas12 -e $FARM_LOG_DIR -o $FARM_LOG_DIR -N $JOBNAME jobexec.sh
+	    qsub -V -q clas12 -e $FARM_LOG_DIR -o $FARM_LOG_DIR -N $JOBNAME jobexec.sh
 	elif [ $batchmode == "npc" ]
 	then
+	    npc_logfile=$FARM_LOG_DIR"/"$JOBNAME".log"
 	    echo "npcing"
-	    ./jobexec.sh &
+	    ./jobexec.sh 2>&1 $npc_logfile & 
 	fi
-	sleep 1
+	sleep 5
     done
 done
