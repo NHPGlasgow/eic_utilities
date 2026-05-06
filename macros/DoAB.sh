08b99f74371d0871dbdce12aec2e1df71f521efa
garypenman <gary.penman@glasgow.ac.uk>
2026-05-06 17:04:53 +0100

    Update DoAB.sh, third stage no longer needed, abconv default output is now hepmc3.tree.root

--- macros/DoAB.sh
+++ macros/DoAB.sh
@@ -4,7 +4,7 @@
 mkdir -p "rootfiles"
 mkdir -p "afterburned"
 
-if [ -z "$1"]
+if [ -z "$1" ]
 then
     abconfig="0"
 else
@@ -14,42 +14,44 @@ fi
 for f in *.hepmc; do
     # remove extension and append .root
     base="${f%.hepmc}"
-    new="${base}.hepmc3.tree.root"
+    new="rootfiles/${base}.hepmc3.tree.root"
     
-    ab="ab_${base}"
-    abfile="ab_${base}.hepmc"
-    newab="ab_${base}.hepmc3.tree.root"
+    ab="afterburned/ab_${base}"
+    abfile="afterburned/ab_${base}.hepmc"
+    newab="afterburned/ab_${base}.hepmc3.tree.root"
 
     # Example conversion command (replace with your actual converter)
     # e.g.: HepMC2Root "$f" "$new"
     echo "Converting $f -> $new,"
     echo "Afterburning $f -> $ab,"
-    echo "Converting $abfile -> $newab."
+    #echo "Converting $abfile -> $newab."
     
-    if [ ! -f "rootfiles/"$new ]
+    if [ ! -f $new ]
     then
 	~/hepmc3ascii2root/install/bin/hepmc3ascii2root "$f" "$new"
     else
 	echo "$new exists, skipping conversion"
     fi
+
     
-    if [ ! -f "afterburned"/$abfile ]
+    if [ ! -f $abfile ]
     then
 	#echo "abconv -p $abconfig $f -o $ab"
-	abconv -p $abconfig $f -o $ab
+	abconv -p $abconfig $f -o $ab --plot-off
     else
 	echo "$abfile exists, skipping abconv"
     fi
 
-    if [ ! -f "afterburned/"$newab ]
-    then
-	~/hepmc3ascii2root/install/bin/hepmc3ascii2root "$abfile" "$newab"
-    else
-	echo "$new exists, skipping conversion"
-    fi
+    
+#    if [ ! -f $newab ]
+#    then
+#	~/hepmc3ascii2root/install/bin/hepmc3ascii2root "$abfile" "$newab"
+#    else
+#	echo "$newab exists, skipping conversion"
+#    fi
 
 done
 
-mv ab_* afterburned/
-mv *.root rootfiles/
+#mv ab_* afterburned/
+#mv *.root rootfiles/
  
