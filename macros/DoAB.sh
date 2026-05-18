f5ac7827ff5b50d66b6ab150195a4cddfe27051a
garypenman <gary.penman@glasgow.ac.uk>
2026-05-18 16:07:53 +0100

    afterburner update

--- macros/DoAB.sh
+++ macros/DoAB.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# Convert all .hepmc files in the current directory to .root
+
+# Convert all .hepmc files in the current directory to .hepmc3.tree.root
+# to rootfiles/
+# Afterburn all .hepmc files in the current directory
+# to afterburned/
 
 mkdir -p "rootfiles"
 mkdir -p "afterburned"
@@ -17,14 +21,12 @@ for f in *.hepmc; do
     new="rootfiles/${base}.hepmc3.tree.root"
     
     ab="afterburned/ab_${base}"
-    abfile="afterburned/ab_${base}.hepmc"
-    newab="afterburned/ab_${base}.hepmc3.tree.root"
+    newab="${ab}.hepmc3.tree.root"
 
     # Example conversion command (replace with your actual converter)
     # e.g.: HepMC2Root "$f" "$new"
     echo "Converting $f -> $new,"
     echo "Afterburning $f -> $ab,"
-    #echo "Converting $abfile -> $newab."
     
     if [ ! -f $new ]
     then
@@ -34,24 +36,14 @@ for f in *.hepmc; do
     fi
 
     
-    if [ ! -f $abfile ]
+    if [ ! -f $newab ]
     then
-	#echo "abconv -p $abconfig $f -o $ab"
 	abconv -p $abconfig $f -o $ab --plot-off
     else
 	echo "$abfile exists, skipping abconv"
     fi
 
     
-#    if [ ! -f $newab ]
-#    then
-#	~/hepmc3ascii2root/install/bin/hepmc3ascii2root "$abfile" "$newab"
-#    else
-#	echo "$newab exists, skipping conversion"
-#    fi
 
 done
-
-#mv ab_* afterburned/
-#mv *.root rootfiles/
  
